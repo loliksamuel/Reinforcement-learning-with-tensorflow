eee01e07d7c53f4e303e3c1689fd50bc60fa8a74
Samuel Cardonis <o528060707@gmail.com>
2019-08-11 09:12:39 +0300

    clean

--- contents/1_command_line_reinforcement_learning/treasure_on_right.py
+++ contents/1_command_line_reinforcement_learning/treasure_on_right.py
@@ -12,23 +12,38 @@ import time
 
 np.random.seed(2)  # reproducible
 
+ACTIONS       = ['left', 'right']     # available actions
+MAX_EPISODES = 2    # maximum episodes (like epochs)
+N_STATES     = 6    # the length of the 1 dimensional world has 6 stairs
+EPSILON      = 0.9  # greedy police
+ALPHA        = 0.1  # learning rate
+GAMMA        = 0.9  # discount factor
+SLEEP_TIME   = 0.3  # fresh time for one move
 
-N_STATES = 6   # the length of the 1 dimensional world
-ACTIONS = ['left', 'right']     # available actions
-EPSILON = 0.9   # greedy police
-ALPHA = 0.1     # learning rate
-GAMMA = 0.9    # discount factor
-MAX_EPISODES = 13   # maximum episodes
-FRESH_TIME = 0.3    # fresh time for one move
 
-
-def build_q_table(n_states, actions):
+def init_q_table(n_states, actions):
     table = pd.DataFrame(
         np.zeros((n_states, len(actions))),     # q_table initial values
         columns=actions,    # actions's name
     )
     # print(table)    # show table
     return table
+
+
+def print_env(S, episode, step_counter):
+    # This is how environment be updated
+    env_list = ['-']*(N_STATES-1) + ['T']   # '---------T' our environment
+    #print (env_list)
+    if S == 'terminal':
+        interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
+        print('\r{}'.format(interaction), end='')
+        time.sleep(2)
+        print('\r                                ', end='')
+    else:
+        env_list[S] = 'o'
+        interaction = ''.join(env_list)
+        print('\r{}'.format(interaction), end='')
+        time.sleep(SLEEP_TIME)
 
 
 def choose_action(state, q_table):
@@ -46,10 +61,10 @@ def get_env_feedback(S, A):
     if A == 'right':    # move right
         if S == N_STATES - 2:   # terminate
             S_ = 'terminal'
-            R = 1
+            R = 1# REWARD=1
         else:
             S_ = S + 1
-            R = 0
+            R = 0# REWARD=0
     else:   # move left
         R = 0
         if S == 0:
@@ -59,32 +74,18 @@ def get_env_feedback(S, A):
     return S_, R
 
 
-def update_env(S, episode, step_counter):
-    # This is how environment be updated
-    env_list = ['-']*(N_STATES-1) + ['T']   # '---------T' our environment
-    if S == 'terminal':
-        interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
-        print('\r{}'.format(interaction), end='')
-        time.sleep(2)
-        print('\r                                ', end='')
-    else:
-        env_list[S] = 'o'
-        interaction = ''.join(env_list)
-        print('\r{}'.format(interaction), end='')
-        time.sleep(FRESH_TIME)
 
 
 def rl():
     # main part of RL loop
-    q_table = build_q_table(N_STATES, ACTIONS)
     for episode in range(MAX_EPISODES):
         step_counter = 0
-        S = 0
+        S = 0 # S= STATE, or current stair
         is_terminated = False
-        update_env(S, episode, step_counter)
+        print_env(S, episode, step_counter)
         while not is_terminated:
 
-            A = choose_action(S, q_table)
+            A = choose_action(S, q_table) #A=Action (left or right)
             S_, R = get_env_feedback(S, A)  # take action & get next state and reward
             q_predict = q_table.loc[S, A]
             if S_ != 'terminal':
@@ -96,12 +97,18 @@ def rl():
             q_table.loc[S, A] += ALPHA * (q_target - q_predict)  # update
             S = S_  # move to next state
 
-            update_env(S, episode, step_counter+1)
+            print_env(S, episode, step_counter + 1)
             step_counter += 1
     return q_table
 
 
+
 if __name__ == "__main__":
+
+    q_table = init_q_table(N_STATES, ACTIONS)
+    print('\r\nQ-table initialized:\n')
+    print(q_table)
+
     q_table = rl()
-    print('\r\nQ-table:\n')
+    print('\r\nQ-table end result:\n')
     print(q_table)
